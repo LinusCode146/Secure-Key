63e41fd9f95ed02f95ed9c0aaa38a4255b1d4f6b
Marius Schmidt <marius.schmidt@denktmit.de>
2024-04-23 21:00:29 +0200

    deleted secret

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -6,7 +6,6 @@ import {Adapter} from "next-auth/adapters";
 
 export const authOptions = {
     adapter: PrismaAdapter(prisma) as Adapter,
-    secret: process.env.AUTH_SECRET,
     providers: [
         GoogleProvider({
             clientId: process.env.GOOGLE_CLIENT_ID as string,
